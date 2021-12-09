8a77547df0b5f0116e3765197f32ec9b43172499
Navoznov Ivan <ivan.navoznov@gmail.com>
2021-12-09 22:42:32 +0300

    Day 09 part 2 is solved

--- Day09/main.py
+++ Day09/main.py
@@ -1,8 +1,8 @@
-import math
 import numpy as np
 
 lines = open("Day09/input.txt").read().split('\n')
 map = [[int(x) for x in line] for line in lines]
+deltas = [(0, -1), (0, 1), (-1, 0), (1, 0)]
 
 
 def add_border(map, border_value):
@@ -25,7 +25,6 @@ def get_map_point(map, point, delta):
 
 
 def is_lowest(map, x, y):
-    deltas = [(0, -1), (0, 1), (-1, 0), (1, 0)]
     current_value = map[y][x]
     for dx, dy in deltas:
         if current_value >= map[y + dy][x + dx]:
@@ -47,14 +46,37 @@ def get_lowest_points(map):
     return result
 
 
-bordered_map = add_border(map, 42)
+bordered_map = add_border(map, 9)
 lowest_points = get_lowest_points(bordered_map)
 
 # part 1
-
 risk_levels = [bordered_map[y][x] + 1 for x, y in lowest_points]
 print(sum(risk_levels))
 
-#part 2
 
-pass
+#part 2
+def get_high_neighbors_of_point(map, x, y):
+    all_neightbors = [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
+    current_value = map[y][x]
+    return [(x, y) for x, y in all_neightbors if map[y][x] != 9 and map[y][x] > current_value]
+
+
+def get_high_neighbors(map, x, y, existing_neighbors):
+    neighbors = get_high_neighbors_of_point(map, x, y)
+    existing_neighbors = existing_neighbors | set(neighbors)
+    for xn, yn in neighbors:
+        existing_neighbors = get_high_neighbors(map, xn, yn, existing_neighbors)
+
+    return existing_neighbors
+
+
+def get_basin_size(map, lowest_point):
+    x, y = lowest_point
+    neighbors = get_high_neighbors(map, x, y, set())
+    return len(neighbors) + 1
+
+
+basins = [get_basin_size(bordered_map, p) for p in lowest_points]
+basins = sorted(basins)
+a, b, c = basins[-3:]
+print(a * b * c)
