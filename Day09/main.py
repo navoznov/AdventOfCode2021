0bdaa2043edcb9cd19ab9b8f56e2d3c689ca6513
Navoznov Ivan <ivan.navoznov@gmail.com>
2021-12-09 20:55:51 +0300

    formatting

--- Day09/main.py
+++ Day09/main.py
@@ -4,6 +4,7 @@ import numpy as np
 lines = open("Day09/input.txt").read().split('\n')
 map = [[int(x) for x in line] for line in lines]
 
+
 def add_border(map, border_value):
     x_size = len(map[0])
     map = np.array(map)
@@ -16,16 +17,18 @@ def add_border(map, border_value):
         map[i].insert(0, border_value)
     return map
 
+
 def get_map_point(map, point, delta):
     x, y = point
-    dx,dy = delta if delta else (0, 0)
-    return map[y+dy][x+dx]
+    dx, dy = delta if delta else (0, 0)
+    return map[y + dy][x + dx]
+
 
 def is_lowest(map, x, y):
     deltas = [(0, -1), (0, 1), (-1, 0), (1, 0)]
     current_value = map[y][x]
     for dx, dy in deltas:
-        if current_value >= map[y+dy][x+dx]:
+        if current_value >= map[y + dy][x + dx]:
             return False
 
     return True
@@ -39,9 +42,11 @@ def get_lowest_points(map):
     for y in range(1, y_size - 1):
         for x in range(1, x_size - 1):
             if is_lowest(map, x, y):
-               result.append((x, y))
+                result.append((x, y))
 
     return result
+
+
 bordered_map = add_border(map, 42)
 lowest_points = get_lowest_points(bordered_map)
 
